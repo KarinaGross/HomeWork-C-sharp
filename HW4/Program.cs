be12a06a901b23c54c6f3734c61a22afb13ee794
Karina <karina.dragan4uk@yandex.ru>
2022-07-06 20:55:53 +0300

    Начало решения задачи 27

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -5,19 +5,80 @@
 2, 4 -> 16
 */
 
-int DegreeNumber (int number, int degree)
+// int DegreeNumber (int number, int degree)
+// {
+//     int result = 1;
+//     for (int i = 0; i < degree; i++)
+//     {
+//         result *= number;
+//     }
+//     return result;
+// }
+
+// Console.Write("Введите основание степени: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите показатель степени: ");
+// int exp = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(DegreeNumber(num, exp));
+
+
+
+
+
+
+// Задача 27
+/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+int DegreeNumber (int n, int degree)
 {
     int result = 1;
     for (int i = 0; i < degree; i++)
     {
-        result *= number;
+        result *= n;
     }
     return result;
 }
 
-Console.Write("Введите основание степени: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите показатель степени: ");
-int exp = Convert.ToInt32(Console.ReadLine());
+int LenNumber(int n) 
+{
+    int len = 0;
+    while (n > 0)
+    {
+        n = n / 10;
+        len++;
+    }
+    return len;
+}
 
-Console.WriteLine(DegreeNumber(num, exp));
+int SumFigureNumber (int number)
+{
+    int[] array = new int[LenNumber(number)];
+    int result = 0;
+    for (int i = 0; i < LenNumber(number); i++)
+    {
+        if (LenNumber(number) < 3)
+        {
+            array[i] = number / DegreeNumber(10, LenNumber(number) - 1);
+            number %= 10;
+        }
+        else
+        {
+            array[i] = number / DegreeNumber(10, LenNumber(number) - 1);
+            number %= 10;
+        }
+
+        result += array[i];
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} -> {SumFigureNumber(num)}");
+
