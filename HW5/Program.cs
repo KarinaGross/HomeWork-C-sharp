b3529707a94483f54d90da6f1fb6f9d4fbd269d9
Karina <karina.dragan4uk@yandex.ru>
2022-07-10 11:52:16 +0300

    Решение задачи 34

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿/*  Задача 34: 
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateRandomArray (int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int AmountAnEvenNumber (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] myArray = CreateRandomArray(10, 100, 999);
+ShowArray(myArray);
+Console.WriteLine($"-> {AmountAnEvenNumber(myArray)}");
