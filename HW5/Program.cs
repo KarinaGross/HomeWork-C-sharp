7e6c3eaa70bf0481ac53979019a193125ebb298a
Karina <karina.dragan4uk@yandex.ru>
2022-07-10 12:02:13 +0300

    Решение задачи 36

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -5,6 +5,47 @@
 [345, 897, 568, 234] -> 2
 */
 
+// int[] CreateRandomArray (int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int AmountAnEvenNumber (int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) count++;
+//     }
+//     return count;
+// }
+
+// int[] myArray = CreateRandomArray(10, 100, 999);
+// ShowArray(myArray);
+// Console.WriteLine($"-> {AmountAnEvenNumber(myArray)}");
+
+
+/* Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
 int[] CreateRandomArray (int size, int min, int max)
 {
     int[] array = new int[size];
@@ -24,16 +65,16 @@ void ShowArray (int[] array)
     Console.WriteLine();
 }
 
-int AmountAnEvenNumber (int[] array)
+int SumOddPositionNumber (int[] array)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    int result = 0;
+    for (int i = 1; i < array.Length; i+=2)
     {
-        if (array[i] % 2 == 0) count++;
+        result += array[i];
     }
-    return count;
+    return result;
 }
 
-int[] myArray = CreateRandomArray(10, 100, 999);
+int[] myArray = CreateRandomArray(10, -100, 100);
 ShowArray(myArray);
-Console.WriteLine($"-> {AmountAnEvenNumber(myArray)}");
+Console.WriteLine($"-> {SumOddPositionNumber(myArray)}");
