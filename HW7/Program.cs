77755d1c20e3ffae00a88a87d0fb179ef3b832c1
Karina <karina.dragan4uk@yandex.ru>
2022-07-19 15:13:35 +0300

    Задача 50 решена

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -5,45 +5,39 @@
 // 8 7,8 -7,1 9
 
 
-double[,] GetArray (int m, int n)
-{
-    double[,] result = new double[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().NextDouble() * (10 - (-10)) + (-10);
-        }
-    }
-    return result;
-}
+// double[,] GetArray (int m, int n)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble() * (10 - (-10)) + (-10);
+//         }
+//     }
+//     return result;
+// }
 
-void PrintArray(double[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i, j]:f1}\t");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintArray(double[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]:f1}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-Console.Write("Введите количество строк массива: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите количество столбцов массива: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-double[,] array = GetArray(rows, columns);
-PrintArray(array);
-
-
-
-
-
-
+// double[,] array = GetArray(rows, columns);
+// PrintArray(array);
 
 
 
@@ -62,6 +56,66 @@ PrintArray(array);
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+// Условие задачи и пример отличаются по смылсу,поэтому решение ниже составлено согласно условию задачи.
+
+// int[,] GetArray (int m, int n)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(0, 101);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ArrayElementsByIndexes (int[,] Array, int indexRow, int indexColumn)
+// {
+//     if (indexRow < Array.GetLength(0) && indexColumn < Array.GetLength(1))
+//     {
+//         Console.WriteLine($"На этом месте находится элемент {Array[indexRow, indexColumn]}");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Такой позиции в данном массиве нет");
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = GetArray(rows, columns);
+// PrintArray(array);
+
+// Console.WriteLine("Считая, что индексы строк и столбцов считаются с 0,");
+
+// Console.Write("введите строковый индекс: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("введите столбцовый индекс: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// ArrayElementsByIndexes(array, a, b);
+
+
 
 
 
