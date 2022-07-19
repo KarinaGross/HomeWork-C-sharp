15ea744fbfa7cea29dd3786510a883916c2dd0a7
Karina <karina.dragan4uk@yandex.ru>
2022-07-19 15:31:18 +0300

    Задача 50 решена

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -130,22 +130,60 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray (int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 101);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMeanColumn (int[,] array)
+{
+    double result = 0;
+    Console.Write($"Среднее арифметическое элементов каждого столбца: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            result += array[i, j];
+        }
+        Console.Write($"{result}, ");
+        result = 0;
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = GetArray(rows, columns);
+PrintArray(myArray);
+
+ArithmeticMeanColumn(myArray);
