2107f3285642433704c14986ed9ce6cb51fc6841
Karina <karina.dragan4uk@yandex.ru>
2022-07-24 13:28:30 +0300

    задача 60 решена, но нужна проверка

--- HW8/Program.cs
+++ HW8/Program.cs
@@ -167,62 +167,60 @@
 // 56 8 4 24
 // 10 6 24 49
 
-int[,] GetArray (int m, int n, int minValue, int maxValue)
-{
-    int[,] result = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
-        }
-    }
-    return result;
-}
+// int[,] GetArray (int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
 
-void PrintArray(int[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-int[,] MatrixProduct (int[,] array1, int[,] array2)
-{
-    int[,] product = new int[array1.GetLength(0), array1.GetLength(1)];
-    for (int i = 0; i < array1.GetLength(0); i++)
-    {
-        for (int j = 0; j < array1.GetLength(1); j++)
-        {
-            product[i, j] = array1[i,j] * array2[i, j];
-        }
-    }
-    return product;
-}
+// int[,] MatrixProduct (int[,] array1, int[,] array2)
+// {
+//     int[,] product = new int[array1.GetLength(0), array1.GetLength(1)];
+//     for (int i = 0; i < array1.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array1.GetLength(1); j++)
+//         {
+//             product[i, j] = array1[i,j] * array2[i, j];
+//         }
+//     }
+//     return product;
+// }
 
-Console.Write("Введите количество строк матриц: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк матриц: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите количество столбцов матриц: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов матриц: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix1 = GetArray(rows, columns, 0, 10);
-int[,] matrix2 = GetArray(rows, columns, 0, 10);
-PrintArray(matrix1);
+// int[,] matrix1 = GetArray(rows, columns, 0, 10);
+// int[,] matrix2 = GetArray(rows, columns, 0, 10);
+// PrintArray(matrix1);
 
-Console.WriteLine();
-PrintArray(matrix2);
+// Console.WriteLine();
+// PrintArray(matrix2);
 
-Console.WriteLine();
-Console.WriteLine("Произведение матриц: ");
-PrintArray(MatrixProduct(matrix1, matrix2));
-
-
+// Console.WriteLine();
+// Console.WriteLine("Произведение матриц: ");
+// PrintArray(MatrixProduct(matrix1, matrix2));
 
 
 
@@ -231,13 +229,67 @@ PrintArray(MatrixProduct(matrix1, matrix2));
 
 
 
-// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
+
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // массив размером 2 x 2 x 2
-
 // 12(0,0,0) 22(0,0,1)
-
 // 45(1,0,0) 53(1,0,1)
+
+
+// задача решена, но вывод в консоле с примером не сходится. Проверить!
+
+
+// int[,,] GetArray (int m, int n, int p)
+// {
+//     int[,,] result = new int[m, n, p];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             for (int k = 0; k < p; k++)
+//             {
+//                 result[i, j, k] = new Random().Next(10, 100); // посмотреть как можно создать массив неповторяющихся чисел
+//             }
+            
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < inArray.GetLength(2); k++)
+//             {
+//                 Console.Write($"{inArray[i, j, k]}({i}, {j}, {k})\t");
+//             } 
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите значение первого измерения трехмерного массива: "); //подкорректировать команду
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение второго измерения трехмерного массива: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение третьего измерения трехмерного массива: ");
+// int z = Convert.ToInt32(Console.ReadLine());
+
+// int[,,] myArray = GetArray(x, y, z);
+// PrintArray(myArray);
+
+
+
+
+
+
 
 
 
