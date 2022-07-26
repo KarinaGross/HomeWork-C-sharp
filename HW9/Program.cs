e3423b3540224ca44486d9fec48521e237852075
Karina <karina.dragan4uk@yandex.ru>
2022-07-26 23:35:47 +0300

    задача 68 решена

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -41,24 +41,24 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int SumNumbers (int minValue, int maxValue)
-{
-    int sum = 0;
-    for (int i = minValue; i < (maxValue + 1); i++)
-    {
-        sum += i;
-    }
-    return sum;
-}
+// int SumNumbers (int minValue, int maxValue)
+// {
+//     int sum = 0;
+//     for (int i = minValue; i < (maxValue + 1); i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
 
 
-Console.Write("Введите минимальное значение: ");
-int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное значение: ");
+// int M = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите максимально значение: ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимально значение: ");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(SumNumbers(M, N));
+// Console.WriteLine(SumNumbers(M, N));
 
 
 
@@ -69,3 +69,25 @@ Console.WriteLine(SumNumbers(M, N));
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 29
+
+int AkkermanFunction (int m, int n)
+{
+    if (m == 0) return (n + 1);
+    else if ((m > 0) && (n == 0))
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    }
+    else return (n + 1);
+}
+
+Console.Write("Введите минимальное значение: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимально значение: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({M}, {N}) = {AkkermanFunction(M, N)}");
